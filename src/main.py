0970631c91a14284c938db726ed3ee53528f3ef3
EG-isheng <is1048@nyu.edu>
2022-12-19 23:09:18 -0500

    update csv database for flask to reference

--- src/main.py
+++ src/main.py
@@ -7,17 +7,36 @@ Created on Sat Dec 17 02:17:06 2022
 """
 
 import os
+import pandas as pd
 import linkedin_job_search as ljs
 from dotenv import load_dotenv
 load_dotenv()
 
 
-def main():
+def append_to_csv(job_title, job_title_code, mu, sigma):
+    new_dict = dict(job_title = job_title, job_title_code = int(job_title_code), avg = mu, std_dev = sigma)
+    new_data = pd.DataFrame(new_dict, index=[0])
+
+    try:
+        curr_data = pd.read_csv('data/job_dist_parameters.csv')
+        print('Updating Primary DB')
+        updated_data = pd.concat([curr_data, new_data]).reset_index(drop=True)
+        updated_data.drop_duplicates(subset = 'job_title_code', keep='last', inplace=True)
+        updated_data.to_csv('data/job_dist_parameters.csv', index=False, mode='w')
+    except FileNotFoundError:
+        print('Primary DB location does not exist. Creating now.')
+        new_data.to_csv('data/job_dist_parameters.csv', index=False)
+    return True
+
+
+def main(job_title_code):
     email = os.environ.get('email')
     password = os.environ.get('password')
     api = ljs.linkedin_job_search(email, password)
-    api.build_distribution(job_title_code='25190', days=1)
+    df, common_title = api.build_distribution(job_title_code=job_title_code, days=2)
+    mu, sigma = df.mean(), df.std()
+    append_to_csv(common_title, job_title_code, mu, sigma)
 
 
 if __name__ == "__main__":
-    main()
+    main('25190')
