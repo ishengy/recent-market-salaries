c17c1a0659b3ff534d319021045525ebbdcfdba5
EG-isheng <is1048@nyu.edu>
2022-12-25 12:29:18 -0500

    linting + code updates

--- src/main.py
+++ src/main.py
@@ -13,30 +13,60 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def append_to_csv(job_title, job_title_code, mu, sigma):
-    new_dict = dict(job_title = job_title, job_title_code = int(job_title_code), avg = mu, std_dev = sigma)
+def append_to_csv(job_title, job_title_code, mu, sigma, a, n, norm):
+    new_dict = dict(
+        job_title = job_title,
+        job_title_code = int(job_title_code),
+        avg = mu,
+        std_dev = sigma,
+        shape = a,
+        normal_dist = norm,
+        num_salary = n,
+    )
     new_data = pd.DataFrame(new_dict, index=[0])
 
     try:
         curr_data = pd.read_csv('data/job_dist_parameters.csv')
         print('Updating Primary DB')
-        updated_data = pd.concat([curr_data, new_data]).reset_index(drop=True)
-        updated_data.drop_duplicates(subset = 'job_title_code', keep='last', inplace=True)
-        updated_data.to_csv('data/job_dist_parameters.csv', index=False, mode='w')
+        updated_data = pd.concat(
+            [curr_data, new_data]
+        ).reset_index(drop=True)
+
+        updated_data.drop_duplicates(
+            subset='job_title_code',
+            keep='last',
+            inplace=True
+        )
+
+        updated_data.to_csv(
+            'data/job_dist_parameters.csv',
+            index=False,
+            mode='w'
+        )
     except FileNotFoundError:
         print('Primary DB location does not exist. Creating now.')
         new_data.to_csv('data/job_dist_parameters.csv', index=False)
     return True
 
 
-def main(job_title_code):
+def main(job_title_code, limit=-1, experience=None):
     email = os.environ.get('email')
     password = os.environ.get('password')
     api = ljs.linkedin_job_search(email, password)
-    df, common_title = api.build_distribution(job_title_code=job_title_code, days=30)
-    mu, sigma = df.mean(), df.std()
-    append_to_csv(common_title, job_title_code, mu, sigma)
+    df, common_title, a, mu, sigma, n = api.build_distribution(
+        job_title_code=job_title_code,
+        days=30,
+        limit=limit,
+        experience=experience,
+    )
+    normality = True
+    if a != 0:
+        normality = False
+    append_to_csv(common_title, job_title_code, mu, sigma, a, n, normality)
+
+    return df
 
 
 if __name__ == "__main__":
-    main('25190')
+    main(job_title_code='27', limit=450, experience=['2', '3'])
+    # 33, 595, 4201, 2463, 29, 194
